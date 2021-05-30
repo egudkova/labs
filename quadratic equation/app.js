03b894bb0680dddf5fd39f1a2d1a6156477f7722
egudkova <84560718+egudkova@users.noreply.github.com>
2021-05-30 22:43:19 +0500

    Update app.js

--- quadratic equation/app.js
+++ quadratic equation/app.js
@@ -1,12 +1,12 @@
-	var a = prompt("Введите коэффицент а", "");
-	var b = prompt("Введите коэффицент b", "");
-	var c = prompt("Введите коэффицент c", "");
+	var a = prompt("Введите аргумент а", "");
+	var b = prompt("Введите аргумент b", "");
+	var c = prompt("Введите аргумент c", "");
 
 	if (a != 0 && b != 0 && c != 0) {
 		var d = b * b - 4 * a * c;
 
 		if (d > 0) {
-			alert("Дискриминант больше нуля. (два корня)");
+			alert("Дискриминант больше нуля, корня 2");
 			var x1 = -b / ( 2 * a ) - Math.sqrt( d ) / ( 2 * a );
 			var x2 = -b / ( 2 * a ) + Math.sqrt( d ) / ( 2 * a );
 			document.write("x1 = ", x1);
@@ -14,10 +14,10 @@
 			document.write("x2 = ", x2);
 		} else if (d == 0) {
 			var x1 = -b / ( 2 * a );
-			document.write("Дискриминант равен нулю. (два одинаковых корня)");
+			document.write("Дискриминант равен 0");
 			document.write("x1, x2 = ", x1);
 		} else if (d < 0) {
-			alert("Дискриминант отрицательный (Комплексные корни)");
+			alert("Комплексные корни");
 			document.write("x1 = ");
 			document.write("(", -b, "+", "√(", - d, ")","i", ")", "/", 2 * a);
 			document.write ("<br \/>");
@@ -37,7 +37,7 @@
 			document.write("x = ", "√", -c/a);
 		}
 	} else if (a == 0 && b == 0 && c != 0){
-		document.write("Циферка не равна нулю :(");
+		document.write("не равно 0");
 	} else if (c == 0) {
 		document.write("x = ", "0");
 	}
